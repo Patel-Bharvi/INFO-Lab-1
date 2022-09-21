eb75c36846e6196caa359feeec68fac7ae6e4428
Bharvi Patel <patel.bharvin@northeastern.edu>
2022-09-21 00:46:40 -0400

    initial-commit

---
+++ src/MODEL/UIDATA.java
@@ -0,0 +1,243 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package MODEL;
+
+import java.awt.Image;
+import java.io.File;
+import java.util.Date;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author bharv
+ */
+public class UIDATA {
+     
+    private String FirstName;
+    private String LastName;
+    private String Date;
+    private String DOB;
+    private String Age;
+    private String TelNo;
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public String getDate() {
+        return Date;
+    }
+
+    public String getDOB() {
+        return DOB;
+    }
+
+    public String getAge() {
+        return Age;
+    }
+
+    public void setFirstName(String FirstName) {
+        this.FirstName = FirstName;
+    }
+
+    public void setLastName(String LastName) {
+        this.LastName = LastName;
+    }
+
+    public void setDate(String Date) {
+        this.Date = Date;
+    }
+
+    public void setDOB(String DOB) {
+        this.DOB = DOB;
+    }
+
+    public void setAge(String Age) {
+        this.Age = Age;
+    }
+
+    public void setTelNo(String TelNo) {
+        this.TelNo = TelNo;
+    }
+
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+
+    public void setAdd1(String Add1) {
+        this.Add1 = Add1;
+    }
+
+    public void setAdd2(String Add2) {
+        this.Add2 = Add2;
+    }
+
+    public void setCity(String City) {
+        this.City = City;
+    }
+
+    public void setCountry(String Country) {
+        this.Country = Country;
+    }
+
+    public void setAffiliation(String Affiliation) {
+        this.Affiliation = Affiliation;
+    }
+
+    public void setMajor(String Major) {
+        this.Major = Major;
+    }
+
+    public void setCareer(String Career) {
+        this.Career = Career;
+    }
+
+    public void setDegree1(String Degree1) {
+        this.Degree1 = Degree1;
+    }
+
+    public void setDeg1start(String Deg1start) {
+        this.Deg1start = Deg1start;
+    }
+
+    public void setDeg1end(String Deg1end) {
+        this.Deg1end = Deg1end;
+    }
+
+    public void setDegree2(String Degree2) {
+        this.Degree2 = Degree2;
+    }
+
+    public void setDeg2start(String Deg2start) {
+        this.Deg2start = Deg2start;
+    }
+
+    public void setDeg2end(String Deg2end) {
+        this.Deg2end = Deg2end;
+    }
+
+    public void setPhoto(File Photo) {
+        this.Photo = Photo;
+    }
+
+    public String getTelNo() {
+        return TelNo;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public String getAdd1() {
+        return Add1;
+    }
+
+    public String getAdd2() {
+        return Add2;
+    }
+
+    public String getCity() {
+        return City;
+    }
+
+    public String getCountry() {
+        return Country;
+    }
+
+    public String getAffiliation() {
+        return Affiliation;
+    }
+
+    public String getMajor() {
+        return Major;
+    }
+
+    public String getCareer() {
+        return Career;
+    }
+
+    public String getDegree1() {
+        return Degree1;
+    }
+
+    public String getDeg1start() {
+        return Deg1start;
+    }
+
+    public String getDeg1end() {
+        return Deg1end;
+    }
+
+    public String getDegree2() {
+        return Degree2;
+    }
+
+    public String getDeg2start() {
+        return Deg2start;
+    }
+
+    public String getDeg2end() {
+        return Deg2end;
+    }
+
+    public File getPhoto() {
+        return Photo;
+    }
+    private String Email;
+    private String Add1;
+    private String Add2;
+    private String City;
+    private String Country;
+    private String Affiliation;
+    private String Major;
+    private String Career;
+    private String Degree1;
+    private String Deg1start;
+    private String Deg1end;
+    private String Degree2;
+    private String Deg2start;
+    private String Deg2end;
+    private File Photo;
+    
+    public UIDATA() {
+    }
+
+    public void setFirstName(JTextField FNT) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public void setLastName(JTextField LNT) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public void setDate(JTextField DT) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public void setAge(JTextField AT) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public void setDOB(JLabel DOB) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    //public void setDOB(String text) {
+      //  throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    //}
+
+    //public void setAge(String text) {
+      //  throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    //}
+
+    
+    
+    
+}
